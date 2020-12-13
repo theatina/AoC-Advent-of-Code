83c951cbcbf0ff9f0499e3b63e7ae06b9a02e488
Theatina <ct.kylafi@gmail.com>
2020-12-13 08:07:07 +0200

    Day 13 part 1

---
+++ Day 13 Shuttle Search/day13.py
@@ -0,0 +1,28 @@
+'''
+
+Christina-Theano (Theatina) Kylafi
+Advent of Code 2020
+
+Day 13
+Part 1 
+
+'''
+
+input_file = [ i for i in list(open("./inputDay13.txt","r").read().split("\n")) ]
+print(input_file)
+time = int(input_file[0])
+buses = [  int(i) for i in input_file[1].split(",") if i !="x"  ]
+print(buses)
+
+found = False
+counter = 0
+while found==False:
+    temp_time = time+counter
+    for i in buses:
+        if temp_time%i==0:
+            print(f"\nTime: {temp_time} -> bus: {i} -> waiting time: {temp_time-time} -> mult_prod: {(temp_time-time)*i}\n")
+            found = True
+            break
+    
+    counter+=1
+
